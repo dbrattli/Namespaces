8592f575024a9fdcd29b1d313167325f881d2381
Dag Brattli <dag.brattli@cognite.com>
2019-08-17 05:16:55 +0200

    Update comment

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,7 +8,7 @@ namespace CS
         static void Main(string[] args)
         {
             Say.hello("from C#!");
-            // Bar.Say.hello("from C#!"); // Does not work!
+            // Bar.Say.hello("from C#!"); // The name 'Bar' does not exist in the current context!
             Foo.Bar.Say.hello("from C#!");
         }
     }
